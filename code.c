6189763c460139a3fac2d56e2907a0dad15e1710
Yuvraj Singh <ys792608@gmail.com>
2026-03-30 11:48:48 +0530

    Add pyramid star pattern program in code.c
    Implement a program to print a pyramid pattern of stars.

---
+++ code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        // Print leading spaces
+        for (int space = 1; space <= rows - i; space++) {
+            printf(" ");
+        }
+        // Print stars
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
